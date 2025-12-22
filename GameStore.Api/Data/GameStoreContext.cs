e24b79d91cdf2b2c4074fd7eddaa34db33eeb8d0
junho@bobsweep.com <shinjuno123@naver.com>
2025-12-21 17:29:49 -0800

    Entity Framework (ORM) & Data Feeding

---
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -0,0 +1,23 @@
+using GameStore.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options): DbContext(options)
+{
+    public DbSet<Game> games => Set<Game>();
+
+    public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Action" },
+            new { Id = 2, Name = "Adventure" },
+            new { Id = 3, Name = "Role-Playing" },
+            new { Id = 4, Name = "Strategy" },
+            new { Id = 5, Name = "Sports" },
+            new { Id = 6, Name = "Simulation" }
+        );
+    }
+}
